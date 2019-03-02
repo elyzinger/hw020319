740c49e6a0ec5b44a270e4239e4e51e6595d2c4e
elyzinger <45950112+elyzinger@users.noreply.github.com>
2019-03-02 11:14:36 +0200

    Add files via upload

---
+++ Camp.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camp
+{
+    class Camp
+    {
+        private readonly int ID;
+        public int Latitude { get; set; }
+        public int Longtitude { get; set; }
+        public int NumberOfPeople { get; set; }
+        public int NumberOfTents { get; set; }
+        public int NumberOfFlashLights { get; set; }
+        private static int lastCompld = 0;
+        public Camp(int latitude,int longtitude,int numberOfPeople,int numberOfTents,int numberOfFlashLights)
+        {
+            this.Latitude = latitude;
+            this.Longtitude = longtitude;
+            this.NumberOfPeople = numberOfPeople;
+            this.NumberOfTents = numberOfTents;
+            this.NumberOfFlashLights = numberOfFlashLights;
+        }
+        public Camp(int numberOfPeople, int numberOfTents, int numberOfFlashLights)
+        {
+            this.NumberOfPeople = numberOfPeople;
+            this.NumberOfTents = numberOfTents;
+            this.NumberOfFlashLights = numberOfFlashLights;
+        }
+        public static bool operator == (Camp a, Camp b)
+        {
+            if (a.ID == b.ID)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator !=(Camp a, Camp b)
+        {
+                return !(a.ID == b.ID);
+        }
+        public static bool operator >(Camp a, Camp b)
+        {
+            if (a.NumberOfPeople == b.NumberOfPeople)
+            {
+                return false;
+            }
+
+                return (a.NumberOfPeople > b.NumberOfPeople);
+        }
+        public static bool operator <(Camp a, Camp b)
+        {
+            if( a.NumberOfPeople == b.NumberOfPeople)
+            {
+                return false;
+            }
+            return (a.NumberOfPeople > b.NumberOfPeople);
+        }
+        public static Camp operator +(Camp a, Camp b)
+        {
+            return new Camp(a.NumberOfPeople + b.NumberOfPeople, a.NumberOfTents + b.NumberOfTents ,a.NumberOfFlashLights + b.NumberOfFlashLights);
+        }
+
+        public override bool Equals(object obj)
+        {
+            Camp other = obj as Camp;
+            return this == other;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.ID;
+        }
+
+        public override string ToString()
+        {
+            return $"camp detailse: Latitude {Latitude} Longtitude {Longtitude} NumberOfPeople {NumberOfPeople} NumberOfTents {NumberOfTents} NumberOfFlashLights {NumberOfFlashLights}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Camp a = new Camp(3, 2, 3);
+            Camp b = new Camp(6, 3, 4);
+            if (a > b)
+            {
+                Console.WriteLine($"camp a is bigger");
+            }
+            else
+                Console.WriteLine($"camp b is bigger");
+            Console.WriteLine(b > a);
+            Camp c = new Camp(a + b);
+        }
+    }
+}
